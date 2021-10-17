4e6811c9058073f9a2f985c8a58127f0898a6d74
Arron Taylor <t98@arrontaylor.com>
2021-10-17 14:24:00 +0100

    made comments

--- retrieve current sales.py
+++ retrieve current sales.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 from datetime import datetime
-
+from retrieveRarity import rarityTools
 
 def current_sales():
     DH_forsale = []
@@ -25,13 +25,21 @@ def current_sales():
 if __name__ == '__main__':
     now = datetime.now()
     print("now =", now)
-    # dd_mm_YY_H_M
     dt_string = now.strftime("%d_%m_%Y_%H_%M")
+# checks to see if the rarity index exists or if one needs to be created
+    rarity = input('does a rarity index exist for this Project?')
+    if rarity != 'yes' or 'y' or 'Y' or 'Yes':
+        rarityTools('DH_rarity1.xlsx', "https://rarity.tools/darkhorizon/view/")
+
+# function for getting the current sales or obtains an old one for testing
     df = current_sales()
     # df = pd.read_excel('DH_prices15_10_2021_18_31.xlsx')
+
+# reads the rarity index into memory and combines the data into one dataframe
     df1 = pd.read_excel('DH_Rarity.xlsx')
     left_merged = df.merge(df1, how="left", on='ID')
-    left_merged['rarity/eth'] = left_merged.apply(lambda row: row.Rarity /
-                                                  row.Price, axis=1)
-    left_merged = left_merged.sort_values(by='rarity/eth', ascending=False)
-    left_merged.to_excel('DH_prices' + dt_string + '.xlsx', index=False)
+
+# data is analysed and sorted and exported
+    left_merged['rarity/eth'] = left_merged.apply(lambda row: row.Rarity / row.Price, axis=1)
+    finalDf = left_merged.sort_values(by='rarity/eth', ascending=False)
+    finalDf.to_excel('DH_prices' + dt_string + '.xlsx', index=False)
